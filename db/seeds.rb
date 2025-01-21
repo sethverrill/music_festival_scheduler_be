f67392191c711162b322519d2ae6ed9212a81184
sethverrill <verrill.seth@gmail.com>
2025-01-21 10:24:30 -0700

    Create preliminary seeds file with all seeds for expected mvp use

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,62 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.destroy_all
+Venue.destroy_all
+Artist.destroy_all
+Schedule.destroy_all
+
+
+users = User.create!([
+  { first_name: "Zinia",  last_name: "Turington", email: "ledbyzed@zune.com" },
+  { first_name: "Drew", last_name: "Zapato, Jr.", email: "drewskishoeskitwoski@orbitz.org" },
+  { first_name: "Norma", last_name: "Cloghorne", email: "plainbrasstaps@chewy.edu" },
+  { first_name: "Kilgore", last_name: "Trout", email: "itgoesandso@aol.com" }
+])
+
+venues = Venue.create!([
+  { name: "The Double Deuce" },
+  { name: "The Gaslight Cafe" },
+  { name: "Death Star Cantina" },
+  { name: "Buster's Biker Bar" }
+])
+
+artists = Artist.create!([
+  { name: "Stillwater" },
+  { name: "The Wonders" },
+  { name: "The Soggy Bottom Boys" },
+  { name: "The Beets" },
+  { name: "Steel Dragon" },
+  { name: "Mouse Rat" },
+  { name: "Infinite Sorrow" },
+  { name: "The Dewey Cox Band" },
+  { name: "Dr. Teeth and The Electric Mayhem" },
+  { name: "Jem and the Holograms" },
+  { name: "The Rutles" },
+  { name: "Scrantonicity" },
+  { name: "Hedwig and the Angry Inch" },
+  { name: "Ziggy Stardust and the Spiders from Mars" },
+  { name: "Crucial Taunt" },
+  { name: "Fiona and the Firecats" }
+])
+
+schedules = users.map { |user| Schedule.create!(user: user) }
+time_slots = (1..8).to_a
+
+venues.each_with_index do |venue, venue_index|
+  filled_slots = 0
+  time_slots.each do |slot|
+    break if filled_slots == 4
+
+    next if slot.odd? && venue_index.odd?
+
+    artist = artists.shift
+
+    schedule = schedules.sample
+    Show.create!(
+      time_slot: slot,
+      venue: venue,
+      artist: artist,
+      schedule: schedule
+    )
+
+    filled_slots += 1
+  end
+end
