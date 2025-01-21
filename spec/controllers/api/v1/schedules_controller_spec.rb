38384ff6689f00b407b1d617039d3373d8d57dd0
sethverrill <verrill.seth@gmail.com>
2025-01-21 13:35:52 -0700

    test: Add sad path for GET #show method

--- spec/controllers/api/v1/schedules_controller_spec.rb
+++ spec/controllers/api/v1/schedules_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Api::V1::SchedulesController, type: :controller do
     let!(:schedule) { create(:schedule, user: user) }
     let!(:shows) { create_list(:show, 3, schedule: schedule) }
 
-    context 'GET happy path' do
+    context 'GET #show happy path' do
       it 'returns the schedule with shows' do
         get :show, params: { user_id: user.id }
         parsed_response = JSON.parse(response.body, symbolize_names: true)
@@ -14,6 +14,16 @@ RSpec.describe Api::V1::SchedulesController, type: :controller do
         expect(response).to have_http_status(:ok)
         expect(parsed_response[:data][:attributes][:id]).to eq(schedule.id)
         expect(parsed_response[:data][:relationships][:shows][:data].size).to eq(3)
+      end
+    end
+
+    context 'GET #show sad path' do
+      it 'returns a 404 error when user schedule not found' do
+        get :show, params: { user_id: -1 }
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(:not_found)
+        expect(parsed_response[:error]).to eq('Schedule not found')
       end
     end
   end
