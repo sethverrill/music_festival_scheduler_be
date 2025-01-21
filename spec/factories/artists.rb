6aceddef7cca4e3cdfb3c7289e3ff0c740ee9e1a
sethverrill <verrill.seth@gmail.com>
2025-01-21 10:25:54 -0700

    test: Start factories for all current models

---
+++ spec/factories/artists.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :artist do
+    name { "MyString" }
+  end
+end
---
+++ spec/factories/shows.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :show do
+    time_slot { 1 }
+    schedule { nil }
+    venue { nil }
+    artist { nil }
+  end
+end
