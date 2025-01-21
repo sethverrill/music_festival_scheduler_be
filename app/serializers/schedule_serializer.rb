69bebb8515072846b73ed9e74f52a1f21f5b2966
sethverrill <verrill.seth@gmail.com>
2025-01-21 15:21:09 -0700

    Update schedule serializer to map time_slots with logic to be expanded later

--- app/serializers/schedule_serializer.rb
+++ app/serializers/schedule_serializer.rb
@@ -4,4 +4,19 @@ class ScheduleSerializer
   attributes :id, :created_at
 
   has_many :shows
+
+  attribute :time_slot_map do |schedule|
+    (1..8).map do |slot|
+      {
+        time_slot: slot,
+        shows: schedule.shows.select { |show| show.time_slot == slot }.map do |show|
+          {
+            id: show.id,
+            artist: show.artist.name,
+            venue: show.venue.name
+          }
+        end
+      }
+    end
+  end
 end
