b103d7ad013dcd0c5d77e3af321ef081cd0e7c90
sethverrill <verrill.seth@gmail.com>
2025-01-21 13:00:32 -0700

    Testing data

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ module MusicFestivalSchedulerBe
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_paths += Dir[Rails.root.join('lib', '**/')]
+    config.eager_load_paths += Dir[Rails.root.join('lib', '**/')]
 
     # Configuration for the application, engines, and railties goes here.
     #
