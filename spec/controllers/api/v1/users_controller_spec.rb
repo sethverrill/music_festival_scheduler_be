c5a08e4f30219e249078f34c3c5b7c3edf40b10f
sethverrill <verrill.seth@gmail.com>
2025-01-21 13:04:53 -0700

    test: Add sad path for User #show method

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -15,5 +15,15 @@ RSpec.describe Api::V1::UsersController, type: :controller do
         expect(parsed_response[:data][:attributes][:schedule][:id]).to eq(schedule.id)
       end
     end
+
+    context 'sad path' do
+      it 'returns a 404 whe user does not exist' do
+        get :show, params: { id: 998899 }
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(:not_found)
+        expect(parsed_response[:error]).to eq('User not found')
+      end
+    end
   end
 end
