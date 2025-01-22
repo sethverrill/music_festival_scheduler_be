7b1173cec7fbcd0d8767a47e7ea744fbea463183
sethverrill <verrill.seth@gmail.com>
2025-01-22 13:26:33 -0700

    fix: improved Show Serializer to get expected return from requests

--- app/serializers/show_serializer.rb
+++ app/serializers/show_serializer.rb
@@ -3,6 +3,11 @@ class ShowSerializer
 
   attributes :id, :time_slot
 
-  belongs_to :venue
-  belongs_to :artist
+  attribute :artist do |show|
+    { id: show.artist.id, name: show.artist.name }
+  end
+
+  attribute :venue do |show|
+    { id: show.venue.id, name: show.venue.name }
+  end
 end
