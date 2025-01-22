3c5a2df2b52530b50297fd72a9006e0e7cdfbdb9
sethverrill <verrill.seth@gmail.com>
2025-01-22 14:12:58 -0700

    Added destroy dependancy in show model

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,5 @@
 class Show < ApplicationRecord
-  has_many :schedule_shows
+  has_many :schedule_shows, dependent: :destroy
   has_many :schedules, through: :schedule_shows
   belongs_to :venue
   belongs_to :artist
