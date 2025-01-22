ee40e3ff77f619175f35def694b3cceb4beb626f
sethverrill <verrill.seth@gmail.com>
2025-01-22 13:15:06 -0700

    Add spec test for Users index

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,13 +3,32 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, type: :controller do
   describe '#index' do
     let!(:users) { create_list(:user, 5) }
+    let!(:schedules) { users.map { |user| create(:schedule, user: user) } }
+    let!(:shows) { create_list(:show, 5) }
 
-    it 'returns all users' do
+    before do
+      schedules.each { |schedule| schedule.shows << shows.sample }
+    end
+
+    it 'returns all users with their schedules' do
       get :index
       parsed_response = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to have_http_status(:ok)
       expect(parsed_response[:data].size).to eq(5)
+
+      parsed_response[:data].each do |user|
+        expect(user[:attributes][:schedule][:id]).not_to be_nil
+        
+        user[:attributes][:schedule][:shows].each do |show|
+          expect(show).to have_key(:id)
+          expect(show).to have_key(:time_slot)
+          expect(show[:artist]).to have_key(:id)
+          expect(show[:artist]).to have_key(:name)
+          expect(show[:venue]).to have_key(:id)
+          expect(show[:venue]).to have_key(:name)
+        end
+      end
     end
   end
   
