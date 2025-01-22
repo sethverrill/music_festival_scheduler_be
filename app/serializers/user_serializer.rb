71dcab879ca12f245b2ee80a9cad4c61a1e6bd0f
sethverrill <verrill.seth@gmail.com>
2025-01-22 13:21:47 -0700

    Refactor code to lessen chances of nil return

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,9 +4,18 @@ class UserSerializer
   attributes :id, :first_name, :last_name, :email
 
   attribute :schedule do |user|
-    {
-      id: user.schedule&.id,
-      created_at: user.schedule&.created_at
-    }
+    user.schedule&.then do |schedule|
+      {
+        id: schedule.id,
+        shows: schedule.shows.map do |show|
+          {
+            id: show.id,
+            time_slot: show.time_slot,
+            artist: { id: show.artist&.id, name: show.artist&.name },
+            venue: { id: show.venue&.id, name: show.venue&.name }
+          }
+        end
+      }
+    end
   end
 end
